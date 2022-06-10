5db6933dcfff5f99d4ecb53aee16d75d0e672e82
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-06-10 18:10:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,32 +1,67 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
+/ Copyright 2021 NIKITA ERMOLAEV
+#include <map>
 #include <string>
 
-template<typename T, int size>
-class TStack {
+#include "tstack.h"
 
-private:
-    T arr[50];
-    int top;
-  public:
-    TStack() :top(-1) { }
-    T get() const {
-        return arr[top];
+std::string infx2pstfx(std::string inf) {
+  TStack<char, 100> result;
+  TStack<char, 100> stack;
+  int currentNumber = 0;
+  for (int i = 0; i < inf.size(); i++) switch (inf[i]) {
+      case '+':
+      case '-':
+        if (stack.get() == '/' || stack.get() == '*')
+          result.push(stack.getAndPop());
+      case '*':
+      case '/':
+      case '(':
+        stack.push(inf[i]);
+        break;
+      case ')':
+        while (stack.get() != '(') result.push(stack.getAndPop());
+        stack.pop();
+        while (!stack.isEmpty()) {
+          if (stack.get() == '(' || stack.get() == '+' || stack.get() == '-')
+            break;
+          result.push(stack.getAndPop());
+        }
+        break;
+      default:
+        result.push(inf[i]);
+        break;
     }
-    void pop() {
-        if (top >= 0) top--;
-    }
-    bool isEmpty() const {
-        return top == -1;
-    }
-    void push(T item) {
-        if (top < size - 1) arr[++top] = item;
-    }
-    bool isFull() const {
-        return top == size - 1;
-    }
-};
+  while (!stack.isEmpty()) result.push(stack.getAndPop());
+  std::string resString = "";
+  while (!result.isEmpty())
+    resString =
+        result.getAndPop() + (resString.size() == 0 ? "" : " " + resString);
+  printf("\n123: %s\n\n", resString.c_str());
+  return resString;
+}
 
-#endif // INCLUDE_TSTACK_H_
+int eval(std::string pref) {
+  TStack<int, 100> stack;
+  for (int i = 0; i < pref.size(); i++) {
+    if (pref[i] >= '0') stack.push(pref[i] - '0');
+    switch (pref[i]) {
+      case '+':
+        stack.push(stack.getAndPop() + stack.getAndPop());
+        break;
+      case '-': {
+        int tmp = stack.getAndPop();
+        stack.push(stack.getAndPop() - tmp);
+        break;
+      }
+      case '*':
+        stack.push(stack.getAndPop() * stack.getAndPop());
+        break;
+      case '/': {
+        int tmp = stack.getAndPop();
+        stack.push(stack.getAndPop() / tmp);
+        break;
+      }
+    }
+  }
+  return stack.get();
+}
